053461c2f887e4da1ed458b16e4585073b842daa
sirisha <sirishalolgu2811@gmail.com>
2023-08-24 18:46:47 -0700

    imoving the code into root folder

---
+++ src/test/java/com/numpyninja/cluelesscoders/pageobjects/StackPage.java
@@ -0,0 +1,85 @@
+package com.numpyninja.cluelesscoders.pageobjects;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import com.numpyninja.cluelesscoders.utilities.ExcelUtils;
+
+
+
+
+public class StackPage {
+
+	public WebDriver driver ;
+	 List<WebElement> topicList;
+	 List<String> codeTest;
+     String s ="print'hello'";
+		
+	
+	String expected, actual;
+	public String url = "https://dsportalapp.herokuapp.com/login";
+	 By sign_In = By.xpath("//a[text()='Sign in']");
+		By dropdown = By.xpath("//a[@class='nav-link dropdown-toggle']");
+ 	By stack =By.xpath("//a[text()='Stack']");
+		By get_started = By.xpath("//button[text()='Get Started']");
+	 By stack_get =By.xpath("//h5[text()='Stack']/../a[text()='Get Started']");
+	 By listTopic = By.xpath("//a[@class='list-group-item']");
+	 By operation = By.xpath("//a[@class='list-group-item'][text()='Operations in Stack']");
+	 By topicLeft = By.xpath("//li[contains(@class,'list-group-item')]/a");
+	
+	 By try_here =By.xpath("//a[text()='Try here>>>']");
+	 By code_text =By.xpath("//div[contains(@class, 'CodeMirror')]/div/textarea");
+	 By practiceQues = By.xpath("//a[text()='Practice Questions']");
+	 By btnRun = By.xpath("//button[text()='Run']");
+	 By out = By.id("output");
+		
+		
+	public StackPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(this.driver, this);
+	}
+	
+
+	
+public void selectStackList() {
+	driver.findElement(dropdown).click();
+	driver.findElement(stack).click();
+}
+
+public String getPageUrl() {
+	
+	actual=driver.getCurrentUrl();
+	return actual;
+}
+
+public void clickonTopics(String topicLink) throws Exception {
+	JavascriptExecutor js =(JavascriptExecutor) driver;
+	js.executeScript("window.scrollBy(0,500)");
+	List<String>topicArray = List.of(topicLink.split(","));
+	Iterator<String> a = topicArray.iterator();
+	while(a.hasNext()){
+		String topicToClick = a.next();
+		driver.findElement(By.xpath("//a[contains(text(),'"+topicToClick+"')]")).click();
+		driver.findElement(try_here).click();
+		Thread.sleep(1000);
+		driver.findElement(code_text).sendKeys(s);
+		driver.findElement(btnRun).click();
+		Thread.sleep(1000);
+		driver.navigate().back();
+		
+		}
+	}
+	public void clickonTryHere() throws InterruptedException {
+			Thread.sleep(2000);
+		}
+
+	
+
+
+}
---
+++ src/test/java/com/numpyninja/cluelesscoders/testrunner/TestRun.java
@@ -0,0 +1,32 @@
+package com.numpyninja.cluelesscoders.testrunner;
+
+
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+//@RunWith(Cucumber.class) //Junit execution 
+@CucumberOptions(
+		plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json",					
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}, //reporting purpose
+		monochrome=true,  //console output color
+		// tags =  "@Invalid or @Home",//from feature file
+		features = ".//Features",//location of feature files
+		dryRun=false,
+		glue= "com.numpyninja.cluelesscoders.stepdefinitions") 
+public class TestRun extends AbstractTestNGCucumberTests{
+
+	@Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+				
+		return super.scenarios();
+    }
+	
+	
+	
+}
